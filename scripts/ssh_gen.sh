7721f5d82bb48c324ce87e58aaf55fcfa2216ffe
Christoph Linne <info@clinne.de>
2024-01-12 09:13:36 +0100

    add useful scripts

---
+++ scripts/ssh_gen.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+read -p "Please type in a file name: " file_name
+echo
+
+cryptography_algorithms=("dsa" "ecdsa" "ecdsa-sk" "ed25519" "ed25519-sk" "rsa" "Quit")
+
+PS3="Select a cryptography algorithm (or 'Quit' to exit): "
+
+select ITEM in "${cryptography_algorithms[@]}"; do
+    case $ITEM in
+        "Quit")
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            crypto="$ITEM"
+            break
+            ;;
+    esac
+done
+
+file_path="~/.ssh/$file_name"
+ssh-keygen -t "$crypto" -a 100 -f $file_path
+echo "SSH key generated and saved as $file_path"
+
