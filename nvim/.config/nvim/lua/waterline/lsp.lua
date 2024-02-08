c15ff920c8a33a01a36f972ca466634944735a9e
chinne <chinne@chris-mac.local>
2024-02-08 11:57:51 +0100

    update

--- nvim/.config/nvim/lua/waterline/lsp.lua
+++ nvim/.config/nvim/lua/waterline/lsp.lua
@@ -45,9 +45,9 @@ local lspconfig = require('lspconfig')
 vim.g.coq_settings = { auto_start = 'shut-up' }
 
 -- Enable some language servers with the additional completion capabilities offered by coq_nvim
-local servers = { 'html', 'pyright' }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup(require('coq').lsp_ensure_capabilities({
+--local servers = { 'html', 'pyright' }
+--for _, lsp in ipairs(servers) do
+  --lspconfig[lsp].setup(require('coq').lsp_ensure_capabilities({
     -- on_attach = my_custom_on_attach,
-  }))
-end
+  --}))
+--end
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,81 +1,29 @@
+-- Pull in the wezterm API
 local wezterm = require 'wezterm'
-local act = wezterm.action
 
-return {
-  leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000},
-  -- test
-  --leader = { key = 'Caps_Lock', timeout_milliseconds = 1000},
-  keys = {
-    {
-      key = 'c',
-      mods = 'CTRL',
-      action = wezterm.action_callback(function(window, pane)
-        local has_selection = window:get_selection_text_for_pane(pane) ~= ''
-        if has_selection then
-          window:perform_action(
-            act.CopyTo 'ClipboardAndPrimarySelection',
-            pane
-          )
+-- This will hold the configuration.
+local config = {}
 
-          window:perform_action(act.ClearSelection, pane)
-        else
-          window:perform_action(
-            act.SendKey { key = 'c', mods = 'CTRL' },
-            pane
-          )
-        end
-      end),
-    },
-    -- block mode in vim?
-    --{ key = 'v', mods = 'CTRL', action = wezterm.action.Paste },
-    {
-      key = 'f',
-      mods = 'LEADER',
-      action = wezterm.action.TogglePaneZoomState,
-    },
-    {
-      key = '\\',
-      mods = 'LEADER',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = '-',
-      mods = 'LEADER',
-      action = wezterm.action.SplitVertical{ domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'h',
-      mods = 'LEADER',
-      action = act.ActivatePaneDirection 'Left',
-    },
-    {
-      key = 'l',
-      mods = 'LEADER',
-      action = act.ActivatePaneDirection 'Right',
-    },
-    {
-      key = 'k',
-      mods = 'LEADER',
-      action = act.ActivatePaneDirection 'Up',
-    },
-    {
-      key = 'j',
-      mods = 'LEADER',
-      action = act.ActivatePaneDirection 'Down',
-    },
+-- Use config builder object if possible
+if wezterm.config_builder then config = wezterm.config_builder() end
 
-  },
-  --window_decorations = 'None',
-  window_frame = {
+-- Settings
+config.color_scheme = 'Catppuccin Frappe'
+config.font = wezterm.font_with_fallback({
+  { family = "Hack Nerd Font", scale=1.1 },
+  { family = "JetBrainsMono Nerd Font", scale=1.1 },
+})
+config.window_background_opacity = 0.9
+config.window_close_confirmation = "NeverPrompt"
+config.scrollback_lines = 3500
+
+-- Tab bar
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_frame = {
     border_left_width = '0.5cell',
     border_right_width = '0.5cell',
-  },
-  tab_bar_at_bottom = true,
-  enable_scroll_bar = true,
-  scrollback_lines = 3500,
-  adjust_window_size_when_changing_font_size=false,
-  hide_tab_bar_if_only_one_tab = true,
-  color_scheme = 'Catppuccin Frappe',
-  font = wezterm.font('JetBrains Mono'),
-  font_size = 14,
-}
+  }
+
+return config
